8601c3dc428d852cdfba7500bb5fd63c916bbdb3
Camal Cakar <camal_cakar@yahoo.de>
2021-09-03 12:39:25 +0200

    feat(worker)
    add worker data structure for workers and add skeleton functions

--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,23 @@
-// init
+package worker
+
+type Worker struct {
+	Queue queue.Queue
+	Db map[uuid]Task
+	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("I will collect stats")
+}
+
+func (w *Worker) RunTask() {
+	fmt.Println("I will start or stop a task")
+}
+
+func (w *Worker) StartTask() {
+	fmt.Println("I will start a task")
+}
+
+func (w *Worker) StopTask() {
+	fmt.Println("I will stop a task")
+}
