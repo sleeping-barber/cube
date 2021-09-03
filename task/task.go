b32f93f20b69e9c91ce1e0b997e257748d3ab233
Camal Cakar <camal_cakar@yahoo.de>
2021-09-03 12:01:24 +0200

    feat(task) add task data structure

--- task/task.go
+++ task/task.go
@@ -1,1 +1,17 @@
-// init
+package task
+
+type State int
+
+const (
+	Completed State = iota
+	Failed
+	Pending
+	Running
+	Scheduled
+)
+
+type Task struct {
+	ID uuid.UUID
+	Name string
+	State State
+}
