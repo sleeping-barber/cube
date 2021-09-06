8767d162cb1742d3455124f5e4bc51d19b48b455
Camal Cakar <camal_cakar@yahoo.de>
2021-09-06 09:33:58 +0200

    feat(manager)
    basic data structure

--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,12 @@
-// init
+package manager
+
+import "github.com/golang-collections/collections/queue"
+
+type Manager struct {
+	Pending       queue.Queue
+	EventDb       map[string][]TaskEvent
+	TaskWorkerMap map[uuid.UUID]string
+	TaskDb        map[string][]Task
+	WorkerTaskMap map[string][]uuid.UUID
+	Workers       []string
+}
