96e55a8ba26c91cb9d403b1c74842f1907ac9965
Camal Cakar <camal_cakar@yahoo.de>
2021-09-08 22:32:16 +0200

    chore(container)
    add functions for testing the start and stop of a container

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/docker/docker/client"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 	"github.com/sleeping-barber/cube/manager"
 	"github.com/sleeping-barber/cube/node"
 	"github.com/sleeping-barber/cube/task"
 	"github.com/sleeping-barber/cube/worker"
+	"os"
 	"time"
 )
 
@@ -89,4 +91,52 @@ func main() {
 
 	// print Node object
 	fmt.Println(n)
+
+	fmt.Printf("Create a test container\n")
+	dockerTask, createResult := createContainer()
+	if createResult.Error != nil {
+		fmt.Printf("%v\n", createResult.Error)
+		os.Exit(1)
+	}
+
+	time.Sleep(time.Second * 5)
+	fmt.Printf("stopping container %s\n", createResult.ContainerId)
+	_ = stopContainer(dockerTask)
+}
+
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "postgres:13",
+		Env: []string{
+			"POSTGRES_USER=cube",
+			"POSTGRES_PASSWORD=secret",
+		},
+	}
+
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+
+	d := task.Docker{
+		Config: c,
+		Client: dc,
+	}
+
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
+	}
+
+	fmt.Printf("Container %s is running with config %v\n", result.ContainerId, c)
+	return &d, &result
+}
+
+func stopContainer(d *task.Docker) *task.DockerResult {
+	result := d.Stop()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil
+	}
+	fmt.Printf("Container %s has been stopped and removed\n", result.ContainerId)
+	return &result
 }
